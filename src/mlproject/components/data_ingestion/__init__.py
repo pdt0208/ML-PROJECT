b60a1ec5a6d15d099e7e3763f2f2137abcb83e7a
Paddhati Singh Nirwan <paddhatid@gmail.com>
2026-03-08 08:51:11 -0700

    changes in data

--- src/mlproject/components/data_ingestion/__init__.py
+++ src/mlproject/components/data_ingestion/__init__.py
@@ -1,44 +1,47 @@
 import os
 import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
 import pandas as pd
-from dataclasses import dataclass
+from src.mlproject.utils import read_sql_data
+
 from sklearn.model_selection import train_test_split
 
-from mlproject.logger import logging
-from mlproject.exception import CustomException
-from mlproject.utils import read_sql_data
+from dataclasses import dataclass
+
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join("artifacts", "train.csv")
-    test_data_path: str = os.path.join("artifacts", "test.csv")
-    raw_data_path: str = os.path.join("artifacts", "data.csv")
+    train_data_path: str = os.path.join("artifacts","train.csv")
+    test_data_path: str = os.path.join("artifacts","test.csv")
+    raw_data_path: str = os.path.join("artifacts","raw.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
+        self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method")
         try:
-            df = read_sql_data()
-            logging.info("Read the dataset as dataframe")
+            ##reading the data from mysql
+            df=pd.read_csv(os.path.join('notebook/data','raw.csv'))
+            logging.info("Reading completed mysql database")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            logging.info("Data Ingestion is completed")
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-
-            logging.info("Data ingestion completed")
-
-            return (
+            return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+
+
             )
 
+
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
